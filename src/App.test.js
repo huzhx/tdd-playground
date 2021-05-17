a2f1695f9900583fd9083d3e7f18697c39340634
Zhaoxian Hu <huzhx@uci.edu>
2021-05-17 12:23:30 -0700

    udpate App.js and add App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders App component', async () => {
+    render(<App />);
+
+    // wait for the user to resolve
+    // needs only be used in our special case
+    await screen.findByText(/Signed in as/);
+
+    expect(screen.queryByText(/Search for JavaScript/)).toBeNull();
+
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'JavaScript' },
+    });
+
+    expect(screen.getByText(/Search for JavaScript/)).toBeInTheDocument();
+  });
 });
